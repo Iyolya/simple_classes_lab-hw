84d5a7feb0898acaf20efd6ddaace942d2ece383
Richard Bodsworth <richardbodsworth@gmail.com>
2021-02-08 16:25:26 +0000

    MVP Done

---
+++ src/main/java/Printer.java
@@ -0,0 +1,12 @@
+public class Printer {
+
+    private int numOfSheet;
+    private int paperLeft;
+
+    public getNumOfSheets(number){
+        this.volume = number;
+    }
+
+
+
+}
---
+++ src/test/java/PrinterTest.java
@@ -0,0 +1,9 @@
+import org.junit.Before;
+
+public class PrinterTest {
+PrinterTest printerTest;
+
+@Before
+
+
+}
